d6f68a99938cbc6c867ad7219527129f8d999445
2shining <fetrafaneva@gmail.com>
2026-01-22 13:57:30 +0300

    feat(auth): configre routes of userRoutes api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,10 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 
 export const getUserData = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const userId = req.userId; // ✅ vient du middleware JWT
 
-    const user = await userModel.findById({ userId });
+    const user = await userModel.findById(userId);
 
     if (!user) {
       return res.json({ success: false, message: "User not found" });
